28cec3ba8be23ed90f09f2361f889fb7e1bf1677
TÙNG BẤT NGỜ <110364428+tungciu@users.noreply.github.com>
2022-11-16 13:54:50 +0800

    Navigtiton

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout mdrawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mdrawerLayout = findViewById(R.id.drawlayout);
+        navigationView = findViewById(R.id.id_navigtion);
+
+        findViewById(R.id.immenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mdrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        // navigation
+        navigationView.setItemIconTintList(null);
+        NavController navController = Navigation.findNavController(this, R.id.naHostFratment);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/fratmentquanly/DoipasssFragment.java
@@ -0,0 +1,32 @@
+package com.example.myapplication.fratmentquanly;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+public class DoipasssFragment extends Fragment {
+
+    public DoipasssFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_doipasss, container, false);
+    }
+}
