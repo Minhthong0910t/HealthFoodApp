bd550bd0f9a8a0aa887be88fcc3520a43dae88bb
TÙNG BẤT NGỜ <110364428+tungciu@users.noreply.github.com>
2022-11-17 10:22:46 +0800

    tung: upadate navigation drawwer

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 
 import android.os.Bundle;
+import android.view.View;
 
 
 import com.example.myapplication.FRAGMENT.AddLoaiSanPham;
@@ -19,13 +24,17 @@ import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 public class MainActivity extends AppCompatActivity {
     ChipNavigationBar chipNavigationBar;
     FragmentManager fragmentManager;
+
+
+    DrawerLayout mdrawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hover();
         fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        fragmentManager.beginTransaction().replace(R.id.naHostFratment, new HomeFragment()).commit();
         chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
             @Override
             public void onItemSelected(int i) {
@@ -45,16 +54,27 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if(fragment!=null){
                     fragmentManager = getSupportFragmentManager();
-                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                    fragmentManager.beginTransaction().replace(R.id.naHostFratment, fragment).commit();
                 }
             }
         });
+        findViewById(R.id.immenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mdrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        // navigation
+        navigationView.setItemIconTintList(null);
+        NavController navController = Navigation.findNavController(this, R.id.naHostFratment);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
     }
     private void hover(){
 
         chipNavigationBar = findViewById(R.id.chipNavigationbar);
-
+        mdrawerLayout = findViewById(R.id.drawlayout);
+        navigationView = findViewById(R.id.id_navigtion);
 
     }
 
